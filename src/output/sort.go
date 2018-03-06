d94812ada3baac288b9080f0b860740aa9b77231
Mike Kantzer <mbkantzer@gmail.com>
2018-03-06 16:17:14 -0500

    successfully read in the file

--- src/output/sort.go
+++ src/output/sort.go
@@ -1,6 +1,47 @@
+//
 package main
 
-func main() {
-	//open input file
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	//grab Arguments
+	arg := os.Args[1:]
+	fmt.Println(arg)
+	if arg[0] == "--help" {
+		fmt.Println("This command is used to sort and join a bunch of CSV data. Accepts file name/location as argument: will take more than one and use them all together")
+	}
+
+	for _, file := range arg {
+		// open CSV file to read in
+		fi, err := os.Open(file)
+		if err != nil {
+			panic(err)
+		}
+		defer fi.Close()
+
+		//create reader (doesnt require a flush or a close)
+		//default comma is ,
+		r := csv.NewReader(fi)
+		r.FieldsPerRecord = 2
+
+		//Loop down the file
+		for {
+			//read next entry
+			record, err := r.Read()
+			//stop reading at end of file
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(record)
+		}
+	}
 }
